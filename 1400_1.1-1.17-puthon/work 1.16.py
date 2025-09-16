5b5ed93382cd77572640263d1de7b9bce3db7656
UwUHunter <michaileskov200@gmail.com>
2025-09-16 14:13:04 +0300

    Add files via upload

---
+++ 1400_1.1-1.17-puthon/work 1.16.py
@@ -0,0 +1,7 @@
+t = int(input())
+v = int(input())
+x = int(input())
+y = int(input())
+print(f"""
+    a) 5 10     б) 100 {t}      в) {c} 25
+       7 см       1949 {b}         {c} {d}""")
---
+++ 1400_1.1-1.17-puthon/work 1.17.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+x = int(input())
+y = int(input())
+print(f"""
+    a) 2 кг     б) {a} 1      в) {x} {y}
+       13 17       19 {b}         5 {x}""")
