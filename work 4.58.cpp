ebfb24b771e1f2fdd15b55fdba7b53547ba66a15
UwUHunter <michaileskov200@gmail.com>
2025-12-02 18:48:39 +0300

    Delete work 4.58.cpp

--- work 4.58.cpp
+++
@@ -1,9 +0,0 @@
-#include
-void solve458(double a, double b, double c) {
-bool result = (a == b) || (a == c) || (b == c);
-if (result) {
-std::cout << "Среди чисел " << a << ", " << b << ", " << c << " есть хотя бы одна пара равных." << std::endl;
-} else {
-std::cout << "Среди чисел " << a << ", " << b << ", " << c << " нет равных пар." << std::endl;
-}
-}
