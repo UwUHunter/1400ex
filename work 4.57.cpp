5071f0faa61664b3a0bada10a6ab943a59173fb2
UwUHunter <michaileskov200@gmail.com>
2025-12-02 18:48:24 +0300

    Delete work 4.57.cpp

--- work 4.57.cpp
+++
@@ -1,9 +0,0 @@
-#include
-void solve457(int a, int b, int c, int d) {
-bool result = (a % b == c) || (a % b == d);
-if (result) {
-std::cout << "Остаток от деления числа " << a << " на " << b << " равен " << c << " или " << d << std::endl;
-} else {
-std::cout << "Остаток от деления числа " << a << " на " << b << " не равен " << c << " или " << d << std::endl;
-}
-}
